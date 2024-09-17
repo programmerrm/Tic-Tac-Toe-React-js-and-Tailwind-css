92cc6183717e48289a75cb2e36c6fdfda9bdf2bd
Rasel Mahmud <programmerwebrm@gmail.com>
2024-09-17 16:47:01 +0600

    new chnage

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,44 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { calculaterWinner } from "./assets/lib/calculaterWinner";
-
-function Logo() {
-    return (
-        <div className="flex flex-col flex-wrap w-full">
-            <a className="block w-20 h-20" href={"/"}>
-                <img className="w-full h-full rounded-full" src="/images/programmerwebrm.jpg" alt="programmerwebrm" loading="lazy" decoding="async" />
-            </a>
-        </div>
-    );
-}
 
 function PlayerName({ player, setPlayer }) {
     const handleNameChange = (e) => {
-        const name = e.target.name;
+        const { name, value } = e.target;
         setPlayer({
             ...player,
-            [name]: e.target.value,
+            [name]: value,
         });
-    };
+    }
+
     const handleForm = (e) => {
         e.preventDefault();
-        localStorage.setItem('playerNames', JSON.stringify(player));
-    };
-    useEffect(() => {
-        const storedPlayerNames = localStorage.getItem('playerNames');
-        if (storedPlayerNames) {
-            setPlayer(JSON.parse(storedPlayerNames));
-        }
-    }, []);
-    useEffect(() => {
-        const clearStorageTimeout = setTimeout(() => {
-            localStorage.removeItem('playerNames');
-        }, 24 * 60 * 60 * 1000);
+        localStorage.setItem('playerName', JSON.stringify(player));
+    }
 
-        return () => clearTimeout(clearStorageTimeout);
-    }, []);
     return (
-        <form className="flex flex-col flex-wrap gap-3 w-full" onSubmit={handleForm}>
+        <form className="flex flex-col flex-wrap gap-3 w-1/2" onSubmit={handleForm}>
             <div className="flex flex-col flex-wrap w-full">
                 <input
-                    className="text-base font-medium border-none outline-none py-1.5 px-2.5 rounded w-full bg-transparent ring-2 ring-slate-800"
+                    className="text-base font-medium border-none outline-none py-2 px-2.5 rounded w-full bg-transparent ring-2 ring-slate-800"
                     name="firstPlayer"
                     value={player.firstPlayer}
                     onChange={handleNameChange}
                     type="text"
+                    placeholder="First Player"
                 />
             </div>
             <div className="flex flex-col flex-wrap w-full">
                 <input
-                    className="text-base font-medium border-none outline-none py-1.5 px-2.5 rounded border w-full bg-transparent ring-2 ring-slate-800"
+                    className="text-base font-medium border-none outline-none py-2 px-2.5 rounded border w-full bg-transparent ring-2 ring-slate-800"
                     name="secondPlayer"
                     value={player.secondPlayer}
                     onChange={handleNameChange}
                     type="text"
+                    placeholder="Second Player"
                 />
             </div>
             <div className="flex flex-col flex-wrap justify-center items-center w-full">
-                <button className="text-base font-semibold rounded py-1.5 px-5 border-none outline-none ring-2 ring-slate-800 w-[50%]" type="submit">Send</button>
+                <button className="text-base font-semibold rounded py-2.5 px-5 border-none outline-none ring-2 ring-slate-800 w-[50%]" type="submit">Send</button>
             </div>
         </form>
     );
@@ -78,8 +58,8 @@ function WinnerLists({ winner, saveWinner }) {
         }
     }, [winner, saveWinner]);
     return (
-        <div className="flex flex-col flex-wrap justify-center items-center rounded border-2 border-slate-700 py-2.5 col-span-full w-full">
-            <h2 className="text-base lg:text-xl font-bold relative after:absolute after:border-b-2 after:bottom-[-2px] after:left-[-14%] after:w-[120px] after:border-slate-800">
+        <div className="flex flex-col flex-wrap justify-center items-center w-1/2 rounded border-2 border-slate-700">
+            <h2 className="text-base lg:text-xl font-bold relative border-b-2 border-slate-800">
                 Winner Lists
             </h2>
             <ul className="flex flex-col flex-wrap justify-center items-center gap-y-1 mt-2.5 w-full">
@@ -94,7 +74,7 @@ function WinnerLists({ winner, saveWinner }) {
 function NextPlayer({ status }) {
     return (
         <div className="flex flex-col flex-wrap justify-center items-center w-full">
-            <h4 className="text-base font-semibold">Next Player: {status}</h4>
+            <h4 className="text-base font-semibold">Next Player : {status}</h4>
         </div>
     );
 }
@@ -110,7 +90,7 @@ function NewGameStart({ handleNewGameStart }) {
 function WinnerPlayer({ winnerPlayer }) {
     return (
         <div className="flex flex-col flex-wrap justify-center items-center w-full">
-            <h4 className="text-base font-semibold">Winner Player: {winnerPlayer}</h4>
+            <h4 className="text-base font-semibold">Winner Player : {winnerPlayer}</h4>
         </div>
     );
 }
@@ -118,7 +98,7 @@ function WinnerPlayer({ winnerPlayer }) {
 function Square({ onSquaresClick, value }) {
     return (
         <button
-            className="flex flex-col flex-wrap w-full justify-center items-center h-24 text-white text-lg font-semibold rounded border-2 border-gray-600"
+            className="flex flex-col flex-wrap w-full justify-center items-center h-28 lg:h-36 text-white text-lg font-semibold rounded border-2 border-gray-600"
             type="button"
             onClick={onSquaresClick}
         >
@@ -204,46 +184,40 @@ export default function App() {
     });
 
     return (
-        <>
-            <section className="relative top-0 left-0 right-0 py-2.5 w-full">
-                <div className="max-w-screen-2xl container mx-auto px-2.5 lg:px-5 w-full h-full">
-                    <div className="flex flex-col flex-wrap justify-center gap-y-5 w-full h-full">
-                        <div className="grid grid-cols-[28%_58%] lg:grid-cols-3 gap-8 w-full">
-                            <Logo />
-                            <PlayerName player={player} setPlayer={setPlayer} />
-                            <WinnerLists winner={winner} saveWinner={saveWinner} />
-                        </div>
-                        <div className="grid grid-cols-1 lg:grid-cols-3 gap-2.5 justify-center items-center w-full">
-                            <NextPlayer status={status} />
-                            <NewGameStart handleNewGameStart={handleNewGameStart} />
-                            <WinnerPlayer winnerPlayer={winner} />
-                        </div>
-                        <div className="grid grid-cols-1 lg:grid-cols-2 gap-2.5 lg:gap-64 w-full">
-                            <GameBorad
-                                squares={currentSquares}
-                                xIsNext={xIsNext}
-                                onPlay={handlePlay}
-                                player={player}
-                            />
-                            <div className="flex flex-col flex-wrap w-full rounded border-2 border-gray-600">
-                                <div className="flex flex-col flex-wrap items-center  px-2.5 py-2.5 w-full">
-                                    <h2 className="text-xl font-bold relative after:absolute after:border-b-2 after:bottom-[-5px] after:left-[-20%] after:w-[100px] after:border-slate-800">History</h2>
-                                    <div className="flex flex-col flex-wrap items-center mt-2.5 w-full">
-                                        <ol className="flex flex-col flex-wrap gap-y-3">{moves}</ol>
-                                    </div>
+        <section className="relative top-0 left-0 right-0 w-full h-full">
+            <div className="max-w-screen-2xl container mx-auto px-2.5 lg:px-5 w-full h-full">
+                <div className="flex flex-col flex-wrap justify-center items-center gap-y-10 w-full h-full">
+
+                    <div className="grid grid-cols-1 lg:grid-cols-2 place-items-center items-start w-full">
+                        <PlayerName player={player} setPlayer={setPlayer} />
+                        <WinnerLists winner={winner} saveWinner={saveWinner} />
+                    </div>
+
+                    <div className="grid grid-cols-1 lg:grid-cols-3 gap-2.5 justify-center items-center w-full">
+                        <NextPlayer status={status} />
+                        <NewGameStart handleNewGameStart={handleNewGameStart} />
+                        <WinnerPlayer winnerPlayer={winner} />
+                    </div>
+
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-2.5 lg:gap-64 w-full">
+                        <GameBorad
+                            squares={currentSquares}
+                            xIsNext={xIsNext}
+                            onPlay={handlePlay}
+                            player={player}
+                        />
+                        <div className="flex flex-col flex-wrap w-full rounded border-2 border-gray-600">
+                            <div className="flex flex-col flex-wrap items-center  px-2.5 py-2.5 w-full">
+                                <h2 className="text-xl font-bold relative after:absolute after:border-b-2 after:bottom-[-5px] after:left-[-20%] after:w-[100px] after:border-slate-800">History</h2>
+                                <div className="flex flex-col flex-wrap items-center mt-2.5 w-full">
+                                    <ol className="flex flex-col flex-wrap gap-y-3">{moves}</ol>
                                 </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
-            </section>
-            <footer className="relative top-0 left-0 right-0 py-5 w-full">
-                <div className="max-w-screen-2xl container mx-auto px-2.5 lg:px-5 w-full">
-                    <div className="flex flex-col flex-wrap justify-center items-center w-full">
-                        <p className="text-base font-semibold">Created 2024 by <a href="/">programmerwebrm</a></p>
-                    </div>
-                </div>
-            </footer>
-        </>
+            </div>
+        </section>
     );
 }
